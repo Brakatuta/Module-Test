f688c30a25efa0d0ea9ee2664be993088bd4f218
Brakatuta <107749015+Brakatuta@users.noreply.github.com>
2023-07-16 20:13:20 +0200

    Update addResource.lua

--- addResource.lua
+++ addResource.lua
@@ -1,35 +1,7 @@
-function addResource(plr, Resource, Amount)
-	local Inventory = plr.Inventory
-	local MaxStack = Items[Resource].MaxStack
+local TestModule = {}
 
-	repeat
-		for a,b in pairs(Inventory:GetChildren()) do
-			if b.Name == Resource then
-				if b.Value <= MaxStack - 1 then
-					repeat
-						b.Value += 1
-						Amount -= 1
-
-						if Amount == 0 then
-							break
-						end
-					until b.Value == MaxStack
-				end
-			end
-		end
-
-		if Amount >= 1 then
-			local ResourceVal = Instance.new("IntValue", Inventory)
-			ResourceVal.Name = Resource
-
-			repeat
-				ResourceVal.Value += 1
-				Amount -= 1
-
-				if Amount == 0 then
-					break
-				end
-			until ResourceVal.Value == MaxStack
-		end
-	until Amount == 0
+function TestModule:Shout()
+    print(1)
 end
+
+TestModule
